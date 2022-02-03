13a66ca6a7855416446c78e709ec175bb8cec56b
Lisniel Sanchez Morales <lisnielsm@gmail.com>
2022-02-03 11:35:00 -0500

    Se consultó a la API utilizando fetch con la categoría definida en el custom hook

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
-import React, { Fragment, useState } from 'react';
+import React, { Fragment, useEffect, useState } from 'react';
 import Header from './components/Header';
 import Formulario from './components/Formulario';
 
 function App() {
+	
+	// definir la categoria y noticias
 	const [categoria, guardarCategoria] = useState('');
+	const [noticias, guardarNoticias] = useState([]);
+
+	useEffect(() => {
+
+		const consultarAPI = async () => {
+			const API_KEY = 'c4bd5f83cc1348e98e6478bda8e4d76c';
+			const url = `http://newsapi.org/v2/top-headlines?country=cu&category=${categoria}&apiKey=${API_KEY}`;
+
+			const resultado = await fetch(url);
+			const noticias = await resultado.json();
+
+			guardarNoticias(noticias.articles);
+		};
+		consultarAPI();
+	}, [categoria]);
 	
 	return (
 		<Fragment>
