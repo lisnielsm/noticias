358e6b8335c9a5e0ebd177880e5c77129b0ee254
Lisniel Sanchez Morales <lisnielsm@gmail.com>
2022-02-03 11:25:24 -0500

    Se agregó las categorias a mostrar al custom hook

--- src/hooks/useSelect.js
+++ src/hooks/useSelect.js
@@ -1,18 +1,25 @@
 import React, { useState } from 'react';
 
 const useSelect = (stateInicial, opciones) => {
-    
-    const [ state, guardarState ] = useState(stateInicial);
-    
+
+    const [state, guardarState] = useState(stateInicial);
+
     const SelectNoticias = () => (
         <select
             className="browser-default"
+            value={state}
+            onChange={e => guardarState(e.target.value)}
         >
-            <option value="">Seleccione</option>
+            {opciones.map(opcion => (
+                <option
+                    key={opcion.value}
+                    value={opcion.value}
+                >{opcion.label}</option>
+            ))}
         </select>
     )
 
-    return [ state, SelectNoticias ];
+    return [state, SelectNoticias];
 }
- 
+
 export default useSelect;
